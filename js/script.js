c810cb60545bdd618bc02107d1fb68af325d0817
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-10 14:52:05 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 const horas = document.querySelector('#hora')
 const minutos = document.querySelector('#minuto')
 const segundos = document.querySelector('#segundo')
-const horas = document.querySelector('#hora')
-const horas = document.querySelector('#hora')
-const horas = document.querySelector('#hora')
+const dias = document.querySelector('#dia')
+const mes = document.querySelector('#mes')
+const ano = document.querySelector('#ano')
+
+setInterval(relogio, 1000)
+
+function relogio() {
+    hoje = new Date()
+    h = hoje.getHours()
+    m = hoje.getMinutes()
+    s = hoje.getSeconds()
+    d = hoje.getDays()
+    mt = hoje.getMounths()
+}
